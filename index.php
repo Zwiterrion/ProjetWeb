3699e4c88e603af7a46f18095a3e791d4dc30f44
Zwiterrion <etienne.anne@icloud.com>
2014-11-28 12:24:09 +0100

    Second commit

--- index.php
+++ index.php
@@ -2,33 +2,21 @@
 
 namespace fxc; // pas terrible mais c'est le seul moyen...
 require_once 'fxc/xhtml5.php';
-
-//
-// demo phpfxc !
-//
-
-$p1 = P('ceci est un ', Strong('paragraphe'), ' composé de mots, lesquels sont composés de lettres. trop bien.');
-
-$list = Ul( Li('faire les courses')
-	    ,Li('donner à manger à ', Em(class_('uneclasse'), 'popol'))
-	    ,Li('...')
-	   );
-
-$doc = Html( lang('fr')
-	     ,Head( Meta(charset('UTF-8')) )
+require_once 'templates.php';
 
 
-	     ,Body( Header('je suis un ', A(href('http://nyan.cat'), 'header'))
-		    
-		    ,H1('Un paragraphe'), $p1
-		    ,H2('Une liste'), $list
-		    
-		    ,Footer('je suis un pied... de page !!! trop bien.')
-		    )
-	     );
+echo "<!DOCTYPE html>";
+
+echoXml( page("e-Music", '', ''));
 
 
-echoXml($doc);
+
+
+
+
+
+
+
 
 
 
