06ed17b30f36c7322884c34725bdf495b260a5c5
Zwiterrion <etienne.anne@icloud.com>
2015-01-08 19:44:42 +0100

    rien

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ ini_set('display_errors', 'On');
 
 $user = session();
 
-$spotlight = div(id('barre'),form(class_('big_search'),onsubmit('load()'),method('GET'),action('catalog.php'),
-    div(input(type('text'),class_('search'),placeholder('Que voulez-vous ')),
+$spotlight = div(id('barre'),form(class_('big_search'),method('GET'),action('catalog.php'),
+    div(input(type('text'),name('search'),class_('search'),placeholder('Que voulez-vous ')),
       input(type('button'),class_('search_button')))));
 
 echo "<!DOCTYPE html>";
