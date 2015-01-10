7e84eabed337d28c6b5d955070db9b2e229dc401
Zwiterrion <etienne.anne@icloud.com>
2015-01-10 11:42:45 +0100

    Amazon fonctionne

--- amazon/samples/Search/SimpleSearch.php
+++ amazon/samples/Search/SimpleSearch.php
@@ -48,12 +48,60 @@ $search->setResponseGroup(array('Large', 'Small'));
 $formattedResponse = $apaiIO->runOperation($search);
 
 
-$items = $formattedResponse['Items'];
+$items = $formattedResponse['Items']['Item'];
 
+$compteur = 3;
 foreach ($items as $item) {
-	var_dump($item);
+	if($compteur > 0){
+		$features = $item['ItemAttributes'];
+		echo $features['Title'];	
+		$image = $item['MediumImage'];
+		$img = $image['URL']; 
+		echo '<img src='.$img.' alt="Photo Absente"/>';
+		$descrip = ' ';
+		foreach ($features['Feature'] as $var) {
+			$descrip .= '<br/>' .$var;
+		}
+		echo $descrip;
+		$compteur--;
+	}
 }
+/*
+   'Offers' => 
+        array (size=4)
+          'TotalOffers' => int 1
+          'TotalOfferPages' => int 1
+          'MoreOffersUrl' => string 'http://www.amazon.fr/gp/offer-listing/B00HWX1S54%3FSubscriptionId%3DAKIAILHU4BLBWKCGY46Q%26tag%3Dzwitterion-20%26linkCode%3Dsp1%26camp%3D2025%26creative%3D12742%26creativeASIN%3DB00HWX1S54' (length=188)
+          'Offer' => 
+            array (size=2)
+
+
+      'ItemAttributes' => 
+        array (size=16)
+          'Binding' => string 'VÃªtements' (length=10)
+          'Brand' => string 'Yummy Bee' (length=9)
+          'ClothingSize' => string '38/40' (length=5)
+          'Color' => string 'Noir' (length=4)
+          'Department' => string 'femme' (length=5)
+          'EAN' => string '5055738416499' (length=13)
+          'EANList' => 
+            array (size=1)
+              ...
+          'Feature' => 
+            array (size=4)
+              ...
+          'IsAdultProduct' => boolean false
+          'MPN' => string '5055738416499' (length=13)
+          'PackageDimensions' => 
+            array (size=4)
+              ...
+          'PartNumber' => string '5055738416499' (length=13)
+          'ProductGroup' => string 'Apparel' (length=7)
+          'ProductTypeName' => string 'UNDERWEAR' (length=9)
+          'Size' => string '38/40' (length=5)
+          'Title' => string 'Yummy Bee - Nuisette satin + String + dentelle haut bas Lingerie Sexy - Noir rouge violet - grande taille 38-56 (noir,38-40)' (length=124)
 
 
 
+*/
 
